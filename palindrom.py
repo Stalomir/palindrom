8df9f3c75c0defba5d846c8e39a378c4b3f2b0e2
Stalomir <slawomir.kapinos@gmail.com>
2022-05-06 10:11:33 +0200

    Add documentation in palindrom.py

--- palindrom.py
+++ palindrom.py
@@ -1,4 +1,9 @@
 def is_palindrome(word):
+    '''
+    Function returning True/Fals value depending whether the word given is a palindrome or not
+    Arguments:
+    word in a str format
+    '''
     if word == word[::-1]:
         return True
     else: 
@@ -6,3 +11,4 @@ def is_palindrome(word):
        
 result=is_palindrome('kajak')
 print(result)
+help(is_palindrome)
